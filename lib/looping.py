77691b6550f5d7d1fe3bc4afb8af18d3e7cabce4
ayan <ayansaneyabdi@gmail.com>
2026-05-03 19:14:24 +0300

    Fix all python Loops and pass the tests

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,29 @@
 
 def happy_new_year():
     # code goes here!
+    for i in range (10, 0, -1):
+        print(i)
+        print ("Happy New Year!")
     pass
 
 def square_integers(int_list):
     # code goes here!
+    result = []
+    for num in int_list:
+        result.append(num * num) 
+    return result
     pass
 
 def fizzbuzz():
     # code goes here!
+    for i in range(1, 101):
+        # Always check the most restrictive condition first
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
     pass
